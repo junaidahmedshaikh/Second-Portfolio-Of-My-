03e22a23f786618969ee23414aa1c694f3df47d6
Junaid Ahmed Shaikh <junaiddeveloperofficial@gmail.com>
2023-03-06 22:00:28 +0530

    Navbar Error Fix
    Image Error In Navbar Fixed

--- main.js
+++ main.js
@@ -2,21 +2,44 @@ const toggleBtn = document.getElementById('nav-toggle')
 
 toggleBtn.addEventListener('click', toggleNav)
 function toggleNav() {
-    console.log('Its Working',document.getElementById('navbar'))
-
     document.getElementById('navbar').classList.toggle('hidden')
 }
 
+// Dark Mode javascript
 
-// const narbarImageAbout = document.getElementById('#navbarImageAbout');
-// const narbarImageSkills = document.getElementById('#navbarImageSkills');
-// const narbarImageProject = document.getElementById('#navbarImageProject');
-// const narbarImageContact = document.getElementById('#narbarImageContact');
-// narbarImageContact = ` <a href="/contactPage/contactPage.html">
-// <h2>Contact Me</h2>
-// <img id="navbarImageContact" src="https://res.cloudinary.com/difvkvxuy/image/upload/v1673848123/portfolio%20Image/Screenshot_20230116_111300_gxwkcs.png"
-//     alt="">
-// </a>`;
+const darkNlightbtn = document.getElementsByClassName('darkmode');
+
+darkNlightbtn.addEventListener('change', darkMode);
+
+function darkMode(){
+document.body.classList.toggle('dark');
+}
 const primaryHeadingOfHomepage = document.querySelector('.primary-heading-of-homepage');
 const primaryHeadingOfHomepageText = document.createTextNode("Shaikh");
 primaryHeadingOfHomepage.appendChild(primaryHeadingOfHomepageText);
+
+const navbarNode = ` <div class="navbar-links">
+<a class="navbar-card" href="/index.html">
+    <h2>Home</h2>
+    <img id="navbarImageAbout" src=""
+        alt="">
+</a>
+<a class="navbar-card" href="/skillsPage/skillsPage.html">
+    <h2>Skills</h2>
+    <img id="navbarImageSkills" src="https://res.cloudinary.com/difvkvxuy/image/upload/v1673848120/portfolio%20Image/Screenshot_20230116_111227_law3nd.png"
+        alt="">
+</a>
+<a class="navbar-card" href="/projectPage/projectPage.html">
+    <h2>Project</h2>
+    <img id="navbarImageProject" src="https://res.cloudinary.com/difvkvxuy/image/upload/v1673848127/portfolio%20Image/Screenshot_20230116_110632_tqdsmb.png"
+        alt="">
+</a>
+<a class="navbar-card" href="/contactPage/contactPage.html">
+    <h2>Contact Me</h2>
+    <img id="navbarImageContact" src="https://res.cloudinary.com/difvkvxuy/image/upload/v1673848123/portfolio%20Image/Screenshot_20230116_111300_gxwkcs.png"
+        alt="">
+</a>
+</div>`;
+
+// const navbar = document.getElementById('navbar');
+// navbar.insertAdjacentHTML('afterbegin',navbarNode);
